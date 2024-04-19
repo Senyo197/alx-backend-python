f7a4132f8c07d8f095ffe72bc007c7a14c45ca68
Senyo197 <psenyo197@gmail.com>
2024-04-19 04:44:31 +0000

    type annotation

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""A type-annotated function floor which takes a float n as argument and
+returns the floor of the float """
+
 
 def concat(str1: str, str2: str) -> str:
+    """returns a concatenated string"""
     return str1 + str2
